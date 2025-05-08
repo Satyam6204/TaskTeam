fa08174b2798a0898520319a9c68373858c37b86
Satyam6204 <satu45025@gmai.com>
2025-05-08 20:00:29 +0530

    First Commit

---
+++ Satyam_TestCOde/src/main/java/com/nt/service/App.java
@@ -0,0 +1,19 @@
+package com.nt.service;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("""
+        	Hi Sumanta,
+                               I’m excited to share that this is my first project.
+                                I also want to take a moment to appreciate you — you’re a great person,
+                                 incredibly smart, and truly inspiring. Thank you for your support and guidance!
+
+        			Best regards,
+        			Satyam Kumar
+        		                        
+        		      """);
+    }
+}
