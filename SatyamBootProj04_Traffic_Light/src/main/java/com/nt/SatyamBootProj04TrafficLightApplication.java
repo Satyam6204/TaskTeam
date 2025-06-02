4092fe972c5c424cef9aa3854d7480b81f5edd2b
Satyam Kumar <your-email@example.com>
2025-06-02 18:42:30 +0530

    change2

--- SatyamBootProj04_Traffic_Light/src/main/java/com/nt/SatyamBootProj04TrafficLightApplication.java
+++ SatyamBootProj04_Traffic_Light/src/main/java/com/nt/SatyamBootProj04TrafficLightApplication.java
@@ -14,21 +14,17 @@ public class SatyamBootProj04TrafficLightApplication {
 	public static void main(String[] args) {
 		boolean running =true;
 		ApplicationContext ctx =SpringApplication.run(SatyamBootProj04TrafficLightApplication.class, args);
-		 
 
 		while(running) {	  
-	Scanner sc =new Scanner(System.in);	
-		System.out.print("Enter Traffic Light Color\t:");
-		
-		String name = sc.next();
-	
-		    TrafficLight tt= ctx.getBean("tl",TrafficLight.class);
-	  
-		        String msg =tt.Action(name);
-		        		System.out.print(name+" -> "+msg);
-		        				System.out.println();
-		        						System.out.print("Did you want check another Traffic Color So Press Y/N\t:");
-		        			String Again=sc.next();
+	              Scanner sc =new Scanner(System.in);	
+	              			System.out.print("Enter Traffic Light Color\t:");
+	              						String name = sc.next();
+	              			TrafficLight tt= ctx.getBean("tl",TrafficLight.class);
+	              								String msg =tt.Action(name);
+	              									System.out.print(name+" -> "+msg);
+	              										System.out.println();
+	              											System.out.print("Did you want check another Traffic Color So Press Y/N\t:");
+	              													String Again=sc.next();
 		        	if(Again.equalsIgnoreCase("Y")) {
 		        					running = true;
 			
@@ -36,14 +32,13 @@ public class SatyamBootProj04TrafficLightApplication {
 		        			 sc.close();
 		        					running=false;
 		        							System.out.println("Thank You For Visit😎");
-		        	}else {
-		        	     running =false;
-		        	     sc.close();
-		        				System.out.println("Invalid Option Only 'Y' or 'N ");
-		        		}
-				}
-
+		        				}else {
+		        						running =false;
+		        								sc.close();
+		        									System.out.println("Invalid Option Only 'Y' or 'N ");
+		        					}
+							}
+					}
 			}
-		}
 
 	
